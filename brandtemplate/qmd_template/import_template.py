4c35176c585d8010123808e5e8d40572bd5dc432
aplunket <applunket@gmail.com>
2024-06-15 17:53:45 +1000

    updated qmd format

--- brandtemplate/qmd_template/import_template.py
+++ brandtemplate/qmd_template/import_template.py
@@ -1,27 +1,23 @@
-import pkg_resources
+import importlib.resources
 import shutil
-import os
+from pathlib import Path
 
-def copy_files(file_paths, destination='/_extensions/brandtemplate'):
-  """
-  Copy files from the package to the specified destination directory.
 
-  :param file_paths: List of file paths within the package.
-  :param destination: Directory to copy files to (default is current directory).
-  """
-  package_name = __name__.split('.')[0]
-  
-  if not os.path.exists(destination):
-    os.makedirs(destination)
-    
-  for file_path in file_paths:
-    resource = pkg_resources.resource_filename(package_name, file_path)
-    if os.path.isfile(resource):
-        shutil.copy(resource, destination)
-              
 
-'''
-p = importlib.resources.as_file(importlib.resources.files('resources') / 'resource.toml')
-with p as f:
-    my_toml = tomllib.load(f.open('rb'))    # as an example
-'''
+
+def copy_template_files(file_names, destination_dir):
+    """
+    Copy a list of template files from the package to the specified directory.
+
+    :param file_names: List of template file names to copy
+    :param destination_dir: Directory to copy the files to
+    """
+    destination_path = Path(destination_dir)
+    destination_path.mkdir(parents=True, exist_ok=True)
+
+    # Get the package name dynamically
+    package_name = __package__
+
+    for file_name in file_names:
+        with importlib.resources.path(f'{package_name}.template_files', file_name) as template_path:
+            shutil.copy(template_path, destination_path / file_name)
---
+++ todo.md
@@ -0,0 +1,18 @@
+## template
+- create function that copies template from package to folder
+- create function that can update the template with different styles
+- documentation for functions
+- add in new cover / section / thank you pages (and update colours / format if needed)
+
+## great tables
+- create theme for great tables
+- documentation for function
+
+## plotnine
+- create theme from ggplot theme
+- document functions
+
+## overall
+- create readme
+
+
